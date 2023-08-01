268f94811a66b09bf6e6f2ec143a1bc072df0510
이민준/연구원/ID전략솔루션Project(minjun3.lee@lge.com) <minjun3.lee@lge.com>
2023-08-01 23:05:03 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import Customer from './components/Customer'
+import Paper from '@mui/material/Paper';
+import Customer from './components/Customer';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+// import { withStyles } from '@mui/material/styles';
+
+// const styles = theme=>({
+//   root:{
+//     width: '100%',
+//     marginTop: theme.spacing.unit * 3,
+//     overflowX: "auto"
+//   },
+//   table:{
+//     minWidth: 1080
+//   }
+// })
 
 // App.js는 웹사이트 화면에 대한 출력
 
@@ -34,24 +52,25 @@ const customers =[
 
 
 function App() {
-  return (
-    <div>
-      {
-        customers.map(c =>{
+        // const { classes } = this.props;
           return(
-            <Customer
-            key={c.id}
-            id={c.id}
-            image={c.image}
-            name ={c.name}
-            birthday = {c.birthday}
-            gender = {c.gender}
-            job = {c.job}
-            />
+            <Paper>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>번호</TableCell>
+                    <TableCell>이미지</TableCell>
+                    <TableCell>이름</TableCell>
+                    <TableCell>생일</TableCell>
+                    <TableCell>성별</TableCell>
+                    <TableCell>직업</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {customers.map(c=> {return(< Customer key={c.id}id={c.id}image={c.image}name ={c.name}birthday = {c.birthday}gender = {c.gender}job = {c.job}/>)})}
+                </TableBody>
+              </Table>
+            </Paper>
           )
-        })
-      }
-    </div>
-  );
 }
-export default App;
+export default (App);
